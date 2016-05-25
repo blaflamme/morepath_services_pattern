ed7a150c83405bea32fa7fabbcda63ceaf142bb1
Blaise Laflamme <blaise@laflamme.org>
2016-05-25 01:34:42 -0400

    add app service facility

--- morepath_services_pattern/app.py
+++ morepath_services_pattern/app.py
@@ -1,13 +1,19 @@
+from functools import lru_cache
+from dectate import convert_dotted_name
 from more.transaction import TransactionApp
-from more.services import ServiceApp
 
 
-class App(TransactionApp, ServiceApp):
-    pass
+class App(TransactionApp):
+
+    @lru_cache()
+    def service(self, func):
+        if isinstance(func, str):
+            func = convert_dotted_name(func)
+        return func(self)
 
 
 @App.setting_section(section='sqlalchemy')
 def get_sqlalchemy_setting_section():
     return {
-        'sqlalchemy.url': 'sqlite:///morepath_services.db'
+        'sqlalchemy.url': 'sqlite:///morepath_services_pattern.db'
         }
