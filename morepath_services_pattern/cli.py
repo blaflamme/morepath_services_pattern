a330881c88828faf08d7d7bbd2687283b725dfbc
Blaise Laflamme <blaise@laflamme.org>
2016-05-25 02:04:05 -0400

    update cli, still have Cannot lookup without explicit lookup argument

--- morepath_services_pattern/cli.py
+++ morepath_services_pattern/cli.py
@@ -3,6 +3,10 @@ import morepath
 import transaction
 
 from .app import App
+from .generic import (
+    dbsession_service,
+    users_service
+    )
 from .models import(
     Base,
     User
@@ -22,13 +26,14 @@ USERS = [{
 def initdb():
     click.echo('Initialize database...')
     morepath.autoscan()
+    morepath.scan()
+    morepath.commit()
     app = App()
-    app.commit()
     # create database
-    dbsession = app.find_service(name='dbsession')
+    dbsession = dbsession_service(app)
     Base.metadata.create_all(dbsession.bind)
     # add users
-    users = app.find_service(name='users')
+    users = users_service(app)
     with transaction.manager:
         for user in USERS:
             users.add(**user)
